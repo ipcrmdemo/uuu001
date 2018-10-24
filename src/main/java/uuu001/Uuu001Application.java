3e94be7d29cc303d6a77b7790599c672cd53bffb
Matt Cadorette <mcadorette@gmail.com>
2018-10-24 11:06:26 -0500

    Initial commit from Atomist

---
+++ src/main/java/uuu001/Uuu001Application.java
@@ -0,0 +1,12 @@
+package uuu001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uuu001Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uuu001Application.class, args);
+	}
+}
---
+++ src/test/java/uuu001/Uuu001ApplicationTests.java
@@ -0,0 +1,16 @@
+package uuu001;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Uuu001ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
